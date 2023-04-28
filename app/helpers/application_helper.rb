46f621ba09abf46d4836275b3cbdd154a91ed561
TakuyaHarayama <44060633+harayama-developmer@users.noreply.github.com>
2023-04-28 19:28:13 +0000

    課題7b 終了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def percentage(after, before)
+    return "- %" if before.zero?
+
+    num = (after / before.to_f * 100).round
+    "#{num} %"
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,12 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :created_today, -> { where(created_at: Time.current.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: (1.week - 1.day).ago.beginning_of_day..Time.current.end_of_day) }
+  scope :created_last_week, -> { where(created_at: (2.weeks - 1.day).ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
